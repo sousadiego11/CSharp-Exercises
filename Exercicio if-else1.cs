b5ef82ad0a64f6464456c1ed5e762f9788e02775
Diego Sousa <65742620+sousadiego11@users.noreply.github.com>
2020-05-23 13:06:29 -0300

    Update Exercicio if-else1.cs

--- Exercicio if-else1.cs
+++ Exercicio if-else1.cs
@@ -5,20 +5,28 @@ namespace PrimeiroProjetoC{
     class Program{
         static void Main(string[] args)
         {
+           /*
+            o objetivo do código é identificar se a afirmação no campo if é verdadeira 
+            e responder de acordo com a afirmação.
+           */
             int a, b, c, d;
+            
             string[] valores = Console.ReadLine().Split(" ");
             a = int.Parse(valores[0]);
             b = int.Parse(valores[1]);
             c = int.Parse(valores[2]);
             d = int.Parse(valores[3]);
+            
             if (b > c && d > a && a + b < c + d && c > 0 & d > 0 && a % 2 == 0) 
-            {
-                Console.WriteLine("VALORES ACEITOS");
-            }
+            /*
+            se B for maior do que C e se D for maior do que A, 
+            e a soma de C com D for maior que a soma de A e B e se C e D, ambos, 
+            forem positivos e se a variável A for par.
+            */
+            { Console.WriteLine("VALORES ACEITOS");}
+            
             else
-            {
-                Console.WriteLine("VALORES NAO ACEITOS");
-            }
+            {Console.WriteLine("VALORES NAO ACEITOS");}
         }
     }
 }
