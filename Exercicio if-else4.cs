6a4d711d830a471e341af3fdb3ef5561d3178548
Diego Sousa <65742620+sousadiego11@users.noreply.github.com>
2020-05-29 18:33:30 -0300

    Add files via upload

---
+++ Exercicio if-else4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjetoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            string[] coordenadas = Console.ReadLine().Split(" ");
+            x = double.Parse(coordenadas[0], CultureInfo.InvariantCulture);
+            y = double.Parse(coordenadas[1], CultureInfo.InvariantCulture);
+
+            if (x > 0.0 && y > 0.0) {Console.WriteLine("Q1");}
+            else if (x < 0.0 && y > 0.0) {Console.WriteLine("Q2");}
+            else if (x < 0.0 && y < 0.0) {Console.WriteLine("Q3");}
+            else if (x > 0.0 && y < 0.0) {Console.WriteLine("Q4");}
+            else { Console.WriteLine("Origem");}
+
+        }
+    }
+}
