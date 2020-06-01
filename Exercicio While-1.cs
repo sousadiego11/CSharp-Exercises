14f11db64073a0d15f0836219c1b8e000594b280
Diego Sousa <65742620+sousadiego11@users.noreply.github.com>
2020-06-01 20:47:15 -0300

    Add files via upload

---
+++ Exercicio While-1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturasRepetitivas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int soma = 0;
+            while (x != 0)
+            {
+                soma += x;
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+    }
+}
