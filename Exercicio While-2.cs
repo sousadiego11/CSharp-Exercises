3c2e40d2811926274be55ac4a24156190b36f236
Diego Sousa <65742620+sousadiego11@users.noreply.github.com>
2020-06-02 19:59:16 -0300

    Add files via upload

---
+++ Exercicio While-2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturasRepetitivas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo, alcool, gasolina, diesel;
+            codigo = int.Parse(Console.ReadLine());
+            alcool = 0;
+            gasolina = 0;
+            diesel = 0;
+
+            while (codigo != 4)
+            {
+                if (codigo == 1)
+                {
+                    alcool++;
+                }
+                else if (codigo == 2)
+                {
+                    gasolina++;
+                }
+                else if (codigo == 3)
+                {
+                    diesel++;
+                }
+                codigo = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
+        }
+    }
+}
