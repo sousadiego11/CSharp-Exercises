80675d8c62c7c607dbf38f3d77ed36840ef6ee09
Diego Sousa <65742620+sousadiego11@users.noreply.github.com>
2020-06-02 20:15:38 -0300

    Add files via upload

---
+++ Exercicio While-3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturasRepetitivas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, quantidade;
+            double media;
+            idade = int.Parse(Console.ReadLine());
+            media = 0.0;
+            quantidade = 0;
+            
+            while (idade >= 0)
+            {
+                media += idade;
+                quantidade++;
+                idade = int.Parse(Console.ReadLine());
+            }
+            
+            media = media / quantidade;
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
