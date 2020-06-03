0cac6bf1af31f1b0d8fe059dbcffaee6368a8780
Diego Sousa <65742620+sousadiego11@users.noreply.github.com>
2020-06-03 19:36:30 -0300

    Add files via upload

---
+++ Exercicio For-1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tabuada!
+            int N, resultado;
+            N = int.Parse(Console.ReadLine());
+
+            for(int i=1; i<=10; i++)
+            {
+                resultado = i * N;
+                Console.WriteLine(i + " x " + N + " = " + resultado);
+            }
+        }
+    }
+}
