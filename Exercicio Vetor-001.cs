ed880b713d80ec9e16a1b00dc6baf8d7bef65f21
Diego Sousa <65742620+sousadiego11@users.noreply.github.com>
2020-06-16 20:31:02 -0300

    Add files via upload

---
+++ Exercicio Vetor-001.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] nome = new string[n];
+            int[] idade = new int[n];
+            double[] altura = new double[n];
+            
+            // Leitura dos dados
+            for (int i = 0; i<n; i++)
+            {
+                string[] elementos = Console.ReadLine().Split(" ");
+                nome[i] = elementos[0];
+                idade[i] = int.Parse(elementos[1]);
+                altura[i] = double.Parse(elementos[2], CultureInfo.InvariantCulture);
+            }
+            // Calculo da altura média entre as pessoas
+            double alturaMedia = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                alturaMedia += altura[i];
+            }
+            alturaMedia /= n;
+            Console.WriteLine("Altura media: " + alturaMedia.ToString("F2", CultureInfo.InvariantCulture));
+            
+            // Porcentagem de pessoas menores de 16 anos
+            double qntIdade = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (idade[i] < 16)
+                {
+                    qntIdade++;
+                }
+            }
+            double mediaIdade = qntIdade / n * 100.0;
+            Console.Write("Pessoas com menos de 16 anos: " + mediaIdade.ToString("F1", CultureInfo.InvariantCulture) + "%");
+        }
+    }
+}
