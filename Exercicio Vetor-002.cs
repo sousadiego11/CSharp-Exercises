6bbd8bd590fbbca19da3307a35aaf0842c9fa2bd
Diego Sousa <65742620+sousadiego11@users.noreply.github.com>
2020-06-24 19:48:38 -0300

    Add files via upload

---
+++ Exercicio Vetor-002.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int quantidade = 0;
+            double divisao = 0.0;
+            string[] valores = Console.ReadLine().Split(" ");
+            int[] numeros = new int[n];
+            for (int i = 0; i<n; i++)
+            {
+                numeros[i] = int.Parse(valores[i]);
+                divisao = numeros[i] % 2;
+                if (divisao==0)                
+                {
+                    Console.Write(numeros[i] + " ");   
+                    quantidade++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(quantidade);
+        }
+    }
+}
