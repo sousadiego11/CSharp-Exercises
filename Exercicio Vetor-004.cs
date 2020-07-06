fc2cf29a4ae6b339c6de916f4e35e91dfba831cf
Diego Sousa <65742620+sousadiego11@users.noreply.github.com>
2020-07-06 19:36:51 -0300

    Add files via upload

---
+++ Exercicio Vetor-004.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double media = 0.0;
+            string[] vet = Console.ReadLine().Split(" ");
+            double[] valores = new double[n];
+            for (int i = 0; i<n; i++)
+            {
+                valores[i] = double.Parse(vet[i], CultureInfo.InvariantCulture);
+                media += valores[i];
+            }
+            media /= n;
+            Console.WriteLine(media.ToString("F3",CultureInfo.InvariantCulture));
+            for (int i = 0; i < n; i++)
+            {
+                if (valores[i] < media)
+                {
+                    Console.WriteLine(valores[i].ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
